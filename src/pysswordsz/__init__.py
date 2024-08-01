719c5e7d5517f4bee428ac1048640dddc992d341
Sidney Zhang <liangyi@me.com>
2024-08-01 18:11:55 +0800

    update: version 0.1.0.beta

--- src/pysswordsz/__init__.py
+++ src/pysswordsz/__init__.py
@@ -24,17 +24,17 @@ def version():
     print("Licensed under GPL-3.0 license.")
 
 @app.command(help="generate password \t 生成密码")
-def genpass(n:int = 8,
-            xkcd:bool = False,
-            need_upper:bool = True,
-            need_number:bool = True,
-            need_punctuation:bool = True,
-            minia:int = 1,
-            urlsafe:bool = False,
-            xk_mode:str = "pinyin",
-            xk_padding:bool = False,
-            show:bool = True,
-            to_clip:bool = False):
+def genpass(n:Annotated[int,typer.Argument(help="密码位数，建议普通密码至少10位，xkcd密码至少8词",show_default=False)] = 16,
+            xkcd:Annotated[bool,typer.Argument(help="生成XKCD密码")] = False,
+            need_upper:Annotated[bool,typer.Argument(help="需要大写字母")] = True,
+            need_number:Annotated[bool,typer.Argument(help="需要数字")] = True,
+            need_punctuation:Annotated[bool,typer.Argument(help="需要序号")] = True,
+            minia:Annotated[int,typer.Argument(help="各类字符的最小个数")] = 1,
+            urlsafe:Annotated[bool,typer.Argument(help="是否生成链接安全密码")] = False,
+            xk_mode:Annotated[str,typer.Argument(help="xkcd密码生成模式")] = "pinyin",
+            xk_padding:Annotated[bool,typer.Argument(help="xkcd密码中是否使用随机填充")] = False,
+            show:Annotated[bool,typer.Argument(help="是否展示密码")] = True,
+            to_clip:Annotated[bool,typer.Argument(help="是否复制到系统粘贴板")] = False):
     if xkcd :
         pswds = generateXKCDPassword(n=n, type_mode=xk_mode,
                                      padding=xk_padding)
@@ -59,25 +59,30 @@ def cfg_list():
     print(data)
 
 @config.command("rm", help="remove one of config / 删除一个配置")
-def cfg_remove(name:str):
+def cfg_remove(name:Annotated[str,typer.Argument(help="需要移除的配置名")]):
     pszconfig().remove(name=name)
 
 
 @config.command("set", help="set one of config / 设置一个配置")
-def cfg_remove():
-    print("set cofig")
+def cfg_setting(name:Annotated[str,typer.Argument(help="需要添加的配置名")],
+                value:Annotated[str,typer.Argument(help="配置值")]):
+    pszconfig().setting(name=name,value=value)
 
 @passdb.command("build",help="build a vault / 建立密码库")
 def pss_build():
     buildPWDB()
 
 @passdb.command("add", help="add a password to vault / 向密码库添加密码")
-def pss_add(name:str, to:str="default"):
+def pss_add(name:Annotated[str,typer.Argument(help="需要添加的密码名称")],
+            to:Annotated[str,typer.Argument(help="需要添加密码的密码库，默认为最后创建的库",show_default=False)]="default"):
     pwsmanager().add_password(name = name, to = to)
 
 @passdb.command("search",help="search a password in vault / 在密码库中搜索密码")
-def pss_search(name:str, last_more:int = 0, vault:str="default",
-               show:bool = True, to_clip:bool = False):
+def pss_search(name:Annotated[str,typer.Argument(help="要查找的密码名称")], 
+               last_more:Annotated[int,typer.Argument(help="展示旧密码，-1将展示全部，0为不展示，其他正整数为展示长度。")] = 0, 
+               vault:Annotated[str,typer.Argument(help="需要查找的密码库",show_default=False)]="default",
+               show:Annotated[bool,typer.Argument(help="是否展示密码")] = False, 
+               to_clip:Annotated[bool,typer.Argument(help="是否把最新的密码复制到系统粘贴板")] = True):
     mgr = pwsmanager()
     need_all = False if last_more == 0 else True
     data = mgr.search(name = name, all=need_all, vault=vault)
@@ -87,22 +92,38 @@ def pss_search(name:str, last_more:int = 0, vault:str="default",
         if need_all :
             with pl.Config(tbl_rows=-1):
                 hl = last_more if last_more>0 else data.height
-                print(data.head(hl))
+                needPWS = data.head(hl).select(pl.col("uuid","password","createtime"))
+                dddd = needPWS["password"].to_numpy()[0]
+                if show:
+                    print(needPWS)
+                else:
+                    print(needPWS.filter(pl.col("password")!=dddd))
+                if to_clip:
+                    clip.copy(dddd)
         else:
-            print(data)
+            keyname = data.columns
+            for i in keyname:
+                if i !="password":
+                    print("{} :\t{}".format(i,data[i][0]))
+                else:
+                    if show :
+                        print("{} :\t{}".format(i,data[i][0]))
+                    if to_clip :
+                        clip.copy(data[i][0])
 
 @passdb.command("update", help="update a password in vault / 更新密码库中的密码")
-def pss_update(name:str, vault:str="default"):
+def pss_update(name:Annotated[str,typer.Argument(help="要更新的密码名称")], 
+               vault:Annotated[str,typer.Argument(help="指定密码库",show_default=False)]="default"):
     pwsmanager().update(name=name, vault=vault)
 
 @passdb.command("delete",help="delete a password in vault / 删除密码库中的密码")
-def pss_delete(name:str, vault:str="default"):
+def pss_delete(name:Annotated[str,typer.Argument(help="要删除的密码名称")], 
+               vault:Annotated[str,typer.Argument(help="指定密码库",show_default=False)]="default"):
     pwsmanager().delete(name=name, vault=vault)
 
 @passdb.command("list", help="list all passwords in vault / 列出密码库中的所有密码")
 def pss_list(vault:Annotated[str,typer.Argument(help="指定所需密码库",show_default=False)]="default", 
-             add_columns:Annotated[str,typer.Argument(help="增加需要展示的列名，使用`,`隔开。",show_default=False)] = ""
-            ):
+             add_columns:Annotated[str,typer.Argument(help="增加需要展示的列名，使用`,`隔开。",show_default=False)] = ""):
     addcolumns = add_columns.split(",") if add_columns else None
     showall = typer.confirm("是否显示所有信息? --> ")
     needrows = -1 if showall else 16
